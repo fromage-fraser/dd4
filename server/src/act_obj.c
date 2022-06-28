36bf7262e980bcd33b50c7444d912abd6d878dab
mattlawnz <matt@mlmc.nz>
2022-06-28 18:14:32 +1200

    formatting

--- server/src/act_obj.c
+++ server/src/act_obj.c
@@ -2605,7 +2605,7 @@ void do_smelt (CHAR_DATA *ch, char *argument)
        
        /*  ROOM_INDEX_DATA *pRoomIndex; */
         char            arg[MAX_INPUT_LENGTH];
-        char buf [MAX_STRING_LENGTH];
+        char            buf [MAX_STRING_LENGTH];
         OBJ_DATA        *obj;
         bool            found;
         int             percent;
