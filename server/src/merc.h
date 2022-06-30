5d506bca2fca8c7f60771320a8865efe236e22e6
mattlawnz <matt@mlmc.nz>
2022-06-30 21:11:51 +1200

    added smithy gsns

--- server/src/merc.h
+++ server/src/merc.h
@@ -235,9 +235,9 @@ bool    has_tranquility ( CHAR_DATA *ch );
 #define LEVEL_IMMORTAL              L_BUI
 #define LEVEL_HERO                ( LEVEL_IMMORTAL - 1 )
 
-#define MAX_SKILL               507     /* Increased to 507(503), 1299(1295), 385(383), 53(52) for smelt */
-#define MAX_PRE_REQ             1295    /* Added for smelt*/
-#define MAX_SPELL_GROUP         385     /* Added for smelt */
+#define MAX_SKILL               507     /* Increased to 507(503), 1350(1295), 385(383), 53(52) for smelt */
+#define MAX_PRE_REQ             1350    /* Added for smithy*/
+#define MAX_SPELL_GROUP         385     /* Added for smithy */
 #define MAX_GROUPS              53
 #define MAX_FORM_SKILL          73      /* for form skill table */
 #define MAX_VAMPIRE_GAG         26      /* ugly vampire/werewolf hack */
---
+++ server/src/pre_reqs/pre_req-alchemist.c
@@ -0,0 +1,38 @@
+/* Alchemist SubClass 30+ 
+
+  21 Skills
+ */
+
+/* Herb Lore */
+
+{&gsn_group_herb_lore,          &gsn_alchemist_base,    30,     PRE_ALCHEMIST},
+
+{&gsn_classify,                	&gsn_group_herb_lore,   70,     PRE_ALCHEMIST},
+{&gsn_classify,                	&gsn_ranger_base,       60,     PRE_ALCHEMIST},
+
+{&gsn_gather_herbs,             &gsn_group_herb_lore,   80,     PRE_ALCHEMIST},
+{&gsn_gather_herbs,             &gsn_classify,          80,     PRE_ALCHEMIST},
+{&gsn_gather_herbs,             &gsn_ranger_base,       60,     PRE_ALCHEMIST},
+
+/* Alchemy: */
+
+{&gsn_group_alchemy,                &gsn_alchemist_base,       30,     PRE_ALCHEMIST},
+
+{&gsn_hurl,                 	    &gsn_group_alchemist,       30,     PRE_ALCHEMIST},
+{&gsn_fire_flask,                 	&gsn_group_alchemist,       30,     PRE_ALCHEMIST},
+{&gsn_frost_flask,                 	&gsn_group_alchemist,       60,     PRE_ALCHEMIST},
+{&gsn_stun_flask,                 	&gsn_group_alchemist,       70,     PRE_ALCHEMIST},
+{&gsn_blind_flask,                 	&gsn_group_alchemist,       80,     PRE_ALCHEMIST},
+{&gsn_lightning_flask,           	&gsn_group_alchemist,       80,     PRE_ALCHEMIST},
+{&gsn_acid_flask,                 	&gsn_group_alchemist,       90,     PRE_ALCHEMIST},
+{&gsn_bmf_flask,                 	&gsn_group_alchemist,       95,     PRE_ALCHEMIST},
+
+/* Inscription */
+
+{&gsn_group_inscription 	 &gsn_alchemist_base,	30,	PRE_ALCHEMIST},
+
+{&gsn_inscribe,                 	&gsn_group_inscription,       95,     PRE_ALCHEMIST},
+{&gsn_protection,                 	&gsn_group_inscription,       95,     PRE_ALCHEMIST},
+{&gsn_enhancement,            	&gsn_group_inscription,       95,     PRE_ALCHEMIST},
+{&gsn_healing,                 	&gsn_group_inscription,       95,     PRE_ALCHEMIST},
+{&gsn_sanctuary,                 	&gsn_group_inscription,       95,     PRE_ALCHEMIST},
