5b345f2d14f7c7c1d95e07c29a53590e05f22774
mattlawnz <matt@mlmc.nz>
2022-07-14 17:42:58 +1200

    wear logic

--- server/src/magic.c
+++ server/src/magic.c
@@ -3323,7 +3323,7 @@ void spell_identify (int sn, int level, CHAR_DATA *ch, void *vo)
                         case EGO_ITEM_UNCOMMON_SET: /* constant-expression */
                         {
                                 sprintf (buf, "Its uncommon {G[SET BONUS]{x gives the wearer {W%s{x.\n\r",
-                                         skill_table[paf->type].name);
+                                         affect_loc_name (paf->location) );
                                 send_to_char( buf, ch );        /* code */
                                 break;
                         }
