162161f0743ecbf3785e663911348f79290ad3f6
nerble <philosophyofimage@gmail.com>
2026-09-13 14:03:52 +1200

    Nerble undead (#720)
    * Add explicit creature archetype selection to Scribe
    
    * Scribe fix

--- builder/scribe/scribe.pl
+++ builder/scribe/scribe.pl
@@ -1301,6 +1301,23 @@ foreach (0 .. $#mobs) {
         }
     }
 
+    # Optional creature archetype, independent of rank and special functions.
+    # Keep the selected name as text; do not convert it to a table index.
+
+    if (exists $mob{'archetype'}) {
+        my $archetype = defined $mob{'archetype'}
+                ? lc $mob{'archetype'} : '';
+
+        if (!exists $mob_archetype{$archetype}) {
+            print "$err invalid archetype '$archetype'; use one of: "
+                    . join(', ', sort keys %mob_archetype) . "\n";
+            $mob_errors{$mob{'line'}}++;
+        }
+        else {
+            $mob{'archetype'} = $archetype;
+        }
+    }
+
     if ($msg = &get_multiple_flags(\%mob, 'bf', \%mob_bf, '')) {
         print "$err $msg\n";
         $mob_errors{$mob{'line'}}++;
